5222922f2315cd56c57e115e1ac85a35ee9b50db
phaneendra <phaneendra.krishnareddy@gmail.com>
2016-02-11 19:35:18 +0800

    chore(api): fix whitespace and lint errors

--- src/api/lib/swagger-express/security-handlers.js
+++ src/api/lib/swagger-express/security-handlers.js
@@ -1,60 +1,41 @@
-'use strict';
-
-var _ = require('underscore'),
-    httpStatus = require('http-status');
+import _ from 'underscore';
+import httpStatus from 'http-status';
 
 function makeError (code, message) {
-
-    var err = new Error(message || httpStatus[code]);
-    err.statusCode = code;
-    return err;
-
+  var err = new Error(message || httpStatus[code]);
+  err.statusCode = code;
+  return err;
 }
 
 function createSwaggerSecurityOauth2PasswordFlowHandler (verifyToken, authorize) {
+  return function (request, securityDefinition, scopes, cb) {
+    var authorizationHeader = /Bearer (.+)/.exec(request.headers.authorization);
 
-    return function (request, securityDefinition, scopes, cb) {
+    if (!authorizationHeader) {
+      return cb(makeError(httpStatus.UNAUTHORIZED, 'authorization header is missing or in wrong format'));
+    }
 
-        var authorizationHeader = /Bearer (.+)/.exec(request.headers.authorization);
-
-        if (!authorizationHeader) {
-            return cb(makeError(httpStatus.UNAUTHORIZED, 'authorization header is missing or in wrong format'));
+    verifyToken(authorizationHeader[1], function (err, token) {
+      if (err) {
+        return cb(makeError(httpStatus.UNAUTHORIZED));
+      }
+      authorize(token, scopes, function (isAuthorized) {
+        if (!isAuthorized) {
+          return cb(makeError(httpStatus.FORBIDDEN));
         }
-
-        verifyToken(authorizationHeader[1], function (err, token) {
-
-            if (err) {
-                return cb(makeError(httpStatus.UNAUTHORIZED));
-            }
-
-            authorize(token, scopes, function (isAuthorized) {
-
-                if (!isAuthorized) {
-                    return cb(makeError(httpStatus.FORBIDDEN));
-                }
-
-                request.token = token;
-
-                cb();
-
-            });
-
-        });
-
-    };
-
+        request.token = token;
+        cb();
+      });
+    });
+  };
 }
 
 module.exports = function (securityDefinitions, verifyToken, authorize) {
-
-    var handlers = {};
-
-    _.each(securityDefinitions, function (securityDefinition, name) {
-        if (securityDefinition.type === 'oauth2' && securityDefinition.flow === 'password') {
-            handlers[name] = createSwaggerSecurityOauth2PasswordFlowHandler(verifyToken, authorize);
-        }
-    });
-
-    return handlers;
-
+  var handlers = {};
+  _.each(securityDefinitions, function (securityDefinition, name) {
+    if (securityDefinition.type === 'oauth2' && securityDefinition.flow === 'password') {
+      handlers[name] = createSwaggerSecurityOauth2PasswordFlowHandler(verifyToken, authorize);
+    }
+  });
+  return handlers;
 };
